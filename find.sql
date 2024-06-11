a71983313f6edc1923f62192fd1bea7c64acaf14
AliAblak <167646621+AliAblak@users.noreply.github.com>
2024-06-11 18:02:18 +0300

    findtext

---
+++ find.sql
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html leng="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UFT-8">
+    <title>Eğitim Ders</title>
+</head>
+
+<body>
+div
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript info@example.com Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript aliablak@gmail.com Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript
+    Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript Javascript 
+    
+
+    <script type="text/javascript" language="javascript">
+    var valueone    =   document.getElementById("content").innerHTML;
+    var pattern     =   
+
+    </script>
+</body>
+</html>
